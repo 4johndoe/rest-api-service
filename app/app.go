bffc688d7134fc00dc4251fd3ef4578dbdd1f939
bogdan demchenko <bogdandemchenko@gmail.com>
2022-09-06 21:48:14 +0300

    upd

--- app/app.go
+++ app/app.go
@@ -9,8 +9,11 @@ func Start() {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/greet", greet)
-	router.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
+
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
 
 	http.ListenAndServe("localhost:8080", router)
 }
--- app/handlers.go
+++ app/handlers.go
@@ -3,6 +3,7 @@ package app
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -25,4 +26,12 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.Header)
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customers)
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Fprint(w, vars["customer_id"])
+}
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Create customer")
 }
